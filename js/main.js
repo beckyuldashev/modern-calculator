2e1e4e0a0961bfb31ec640e6e78d15185d6ccefb
Beck Yuldashev <yuldashev.bek@gmail.com>
2023-02-06 18:27:07 +0300

    Added logic of calculation

---
+++ js/main.js
@@ -0,0 +1,90 @@
+let runningTotal = 0;
+let buffer = '0';
+let previosOperator;
+
+const screen = document.querySelector('.calculator__screen');
+
+function onBtnClick(value) {
+  if (isNaN(value)) {
+    handleSymbol(value)
+  } else {
+    handleNumber(value);
+  }
+
+  screen.textContent = buffer;
+}
+
+function handleSymbol(symbol) {
+  switch (symbol) {
+    case 'C':
+      buffer = '0';
+      runningTotal = 0;
+      break;
+    case '=':
+      if (previosOperator === null) {
+        return;
+      }
+      flushOperation(parseInt(buffer));
+      previosOperator = null;
+      buffer = runningTotal;
+      runningTotal = 0;
+      break;
+    case '←':
+      if (buffer.length === 1) {
+        buffer = '0';
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    case '−':
+    case '+':
+    case '×':
+    case '÷':
+      handleMath(symbol);
+      break;
+
+  }
+}
+
+function handleMath(symbol) {
+  if (buffer === '0') return;
+
+  const intBuffer = parseInt(buffer);
+
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+
+  previosOperator = symbol;
+  buffer = '0';
+}
+
+function flushOperation(intBuffer) {
+  if (previosOperator === '+') {
+    runningTotal += intBuffer;
+  } else if (previosOperator === '−') {
+    runningTotal += intBuffer;
+  } else if (previosOperator === '×') {
+    runningTotal *= intBuffer;
+  } else if (previosOperator === '÷') {
+    runningTotal /= intBuffer;
+  }
+}
+
+function handleNumber(numberString) {
+  if (buffer === '0') {
+    buffer = numberString;
+  } else {
+    buffer += numberString;
+  }
+}
+
+function init() {
+  document.querySelector('.calculator__operands').addEventListener('click', (e) => {
+    onBtnClick(e.target.textContent);
+  })
+}
+
+init()
